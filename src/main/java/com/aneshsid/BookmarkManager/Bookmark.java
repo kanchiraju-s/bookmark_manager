6aac53c7efa2734983f973660ec56e72626d4aa1
kanchiraju-s <aneeshsiddhartha@gmail.com>
2025-06-16 02:50:11 +0530

    Changes made to the Bookmark pojo

--- src/main/java/com/aneshsid/BookmarkManager/Bookmark.java
+++ src/main/java/com/aneshsid/BookmarkManager/Bookmark.java
@@ -1,5 +1,22 @@
 package com.aneshsid.BookmarkManager;
 
 public class Bookmark {
-	public int id;
+	public String name;
+	public String url;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
 }
